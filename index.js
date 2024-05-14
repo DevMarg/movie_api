77befc666f9aa8d4ed55a75d0a95ed2ec16d78ec
DevMarg <gurau.margarita@gmail.com>
2024-05-14 14:14:36 +0200

    Add Web Server Frameworks & Express
    This commit was updated with:
    -Requires “express”;
    -Contains 2 GET routes: one that returns a JSON object and one that returns a text response;
    -Serves a “documentation.html” file using express.static;
    -Logs all requests using Morgan;
    -Contains an error-handling middleware function

--- index.js
+++ index.js
@@ -1,2 +1,77 @@
-const express = require ("express");
+const express = require ('express');
+const morgan = require('morgan');
 const app = express();
+
+
+let topMovies = [
+    {
+        title: 'Stalker',
+        director: 'Andrei Tarkovsky'
+    },
+    {
+        title: 'La Dolce Vita',
+        director: 'Federico Fellini'
+    },
+    {
+        title: 'Blade Runner',
+        director: 'Ridley Scott'
+    },
+    {
+        title: 'Parasite',
+        director: 'Bong Joon Ho'
+    },
+    {
+        title: 'The Godfather',
+        director: 'Francis Ford Coppola'
+    },
+    {
+        title: 'Vertigo',
+        director: 'Alfred Hitchcock'
+    },
+    {
+        title: 'Taxi Driver',
+        director: 'Martin Scorsese'
+    },
+    {
+        title: 'Mulholland Drive',
+        director: 'David Lynch'
+    },
+    {
+        title: 'Goodfellas',
+        director: 'Martin Scorsesek'
+    },
+    {
+        title: 'Vertigo',
+        director: 'Alfred Hitchcock'
+    }
+
+];
+
+app.use(morgan('common'));
+app.use(express.static('public'));
+
+//Get requests
+app.get('/', (req, res) =>{
+    res.send('Welcome to my Movie_API!');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', {
+        root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+//Error handling
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send('Error');
+});
+
+//Listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080');
+});
+
