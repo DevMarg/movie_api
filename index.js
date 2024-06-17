c72c8847da375468f2933bd003ee4a1075e0bd8f
DevMarg <gurau.margarita@gmail.com>
2024-06-17 15:31:09 +0200

    Removed redundant code
    -Removed redundant code;
    -Added "get all users" function;
    -Added "get user by username" function;

--- index.js
+++ index.js
@@ -13,75 +13,8 @@ const app = express(),
 
 app.use(bodyParser.json());
 
-let users = [
-  {
-    id: 1,
-    name: 'Jessica Drake',
-    favoriteMovies: []
-  },
-  {
-    id: 2,
-    name: 'Ben Cohen',
-    favoriteMovies: ['Stalker']
-  },
-  {
-    id: 3,
-    name: 'Jill Smith',
-    favoriteMovies: ['Vertigo']
-  }
 
-
-];
-
-let movies = [
-  {
-    'Title':'Stalker',
-    'Description':'Two men and their guide, a "stalker", travel through a mysterious and forbidden territory in the wilderness called "the zone".',
-    'Genre': {
-      'Name':'Sci-fi',
-      'Description':'Science fiction is a genre of speculative fiction, which typically deals with alternative ways of life made possible by technological change'
-    },
-    'Director': {
-      'Name':'Andrei Tarkovsky',
-      'Bio':'Andrei Tarkovsky was a Russian film director and screenwriter. Widely considered one of the greatest and most influential directors in cinema history, Tarkovsky studied film at Moscow`s VGIK under filmmaker Mikhail Romm, and subsequently directed his first five features in the Soviet Union: Ivan`s Childhood (1962), Andrei Rublev (1966), Solaris (1972), Mirror (1975), and Stalker (1979).',
-      'Birth': 1932.0
-    },
-    'ImageURL': 'https://images.app.goo.gl/Z7eMRCpyqnZpnEpN9',
-    'Featured':false
-  },
-  {
-    'Title':'La Dolce Vita',
-    'Description':'The film stars Marcello Mastroianni as Marcello Rubini, a tabloid journalist who, over seven days and nights, journeys through the "sweet life" of Rome in a fruitless search for love and happiness.',
-    'Genre': {
-      'Name':'Comedy',
-      'Description':'Comedy is a genre of fiction that consists of discourses or works intended to be humorous or amusing.'
-    },
-    'Director': {
-      'Name':'Federico Fellini',
-      'Bio':'Federico Fellini was an Italian film director and screenwriter. He is known for his distinctive style, which blends fantasy and baroque images with earthiness.',
-      'Birth':1920.0
-    },
-    'ImageURL': 'https://images.app.goo.gl/Bj5nRDd8tVtf4ovL8',
-    'Featured':false
-  },
-  {
-    'Title':'Vertigo',
-    'Description':'A former San Francisco police detective juggles wrestling with his personal demons and becoming obsessed with the woman he has been hired to trail, who may be deeply disturbed.',
-    'Genre': {
-      'Name':'Thriller',
-      'Description':'Thrillers are characterized and defined by the moods they elicit, giving their audiences heightened feelings of suspense, excitement, surprise, anticipation and anxiety.'
-    },
-    'Director': {
-      'Name':'Alfred Hitchcock',
-      'Bio':'Sir Alfred Joseph Hitchcock was an English film director. He is widely regarded as one of the most influential figures in the history of cinema.',
-      'Birth':1899.0
-    },
-    'ImageURL': 'https://images.app.goo.gl/e8N3aDScKaMd559S6',
-    'Featured':false
-  },
-];
-
-//CREATE: Allow new users to register
+//CREATE: Add a user
 app.post('/users', async (req, res) => {
   await Users.findOne({ Username: req.body.Username})
   .then((user) => {
@@ -107,108 +40,29 @@ app.post('/users', async (req, res) => {
   });
 });
 
-//UPDATE: Allow users to update their name by id
-app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-
-  let user = users.find( user => user.id == id );
-
-  if (user) {
-    user.name = updatedUser.name;
-    return res.status(200).json(user);
-  } else {
-    return res.status(400).send('User not found');
-  }
-})
-
-//CREATE: Allow users to add a movie to their list of favorites
-app.post('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  
-
-  let user = users.find( user => user.id == id );
-
-  if (user) {
-    user.favoriteMovies.push(movieTitle)
-    return res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-  } else {
-    return res.status(400).send('User not found');
-  }
-})
-
-//DELETE: Allow users to remove a movie from their list of favorites
-app.delete('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  
-
-  let user = users.find( user => user.id == id );
-
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle)
-    return res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-  } else {
-    return res.status(400).send('User not found');
-  }
-})
-
-//DELETE: Allow existing users to deregister
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-  
-
-  let user = users.find( user => user.id == id );
-
-  if (user) {
-    users = users.filter( user => user.id != id);    
-    return res.status(200).send(`user ${id} has been deleted`);
-  } else {
-    return res.status(400).send('User not found');
-  }
-})
-
-
-
-// READ: Return a list of ALL movies
-app.get('/movies', (req, res) => {
-    return res.status(200).json(movies);
+//READ: Get all users
+app.get('/users', async (req, res) => {
+  await Users.find()
+  .then((users) => {
+    res.status(201).json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
   });
-
-// READ: Return data about a movie
-app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find( movie => movie.Title === title );
-
-  if (movie) {
-    return res.status(200).json(movie);
-  } else {
-    return res.status(400).send('Movie not found');
-  }
 });
 
-// READ: Return data about a genre
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
-
-  if (genre) {
-    return res.status(200).json(genre);
-  } else {
-    return res.status(400).send('Genre not found');
-  }
-});
-
-//READ: Return data about a director
-app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.Director.Name === directorName ).Director;
-
-  if (director) {
-    return res.status(200).json(director);
-  } else {
-    return res.status(400).send('Director not found');
-  }
-});
+//READ: Get a user by username
+app.get('/users/:Username', async (req, res) => {
+  await Users.findOne({ Username: req.params.Username})
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+})
 
 //Error handling
 app.use((err, req, res, next)=>{
