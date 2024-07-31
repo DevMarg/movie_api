ac06517c897bd1cd2a883be828aaded5ef42818a
DevMarg <gurau.margarita@gmail.com>
2024-07-31 16:11:36 +0200

    Updated allowed origins
    Updated allowed origins

--- index.js
+++ index.js
@@ -24,7 +24,19 @@ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifie
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(cors());
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+ origin: (origin, callback) => {
+   if(!origin) return callback(null, true);
+   if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+     return callback(new Error(message ), false);
+   }
+   return callback(null, true);
+ }
+}));
 
 let auth = require('./auth.js')(app);
 
